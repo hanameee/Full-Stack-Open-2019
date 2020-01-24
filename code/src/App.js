00e069ac87952b0768e4d17e2f7f8c0e2cfdd24f
hanameee <hanameee@gmail.com>
2020-01-24 12:06:24 +0900

    2.b - Update App.js, Note component
    Now app provides adding note via form input, and selectively filtering important notes depend on random important property.

--- code/src/App.js
+++ code/src/App.js
@@ -1,57 +1,43 @@
-import React from "react";
-import CourseList from "./Components/part2/ex1-5/CourseList";
+import React, { useState } from "react";
+import Note from "./Components/Note";
 
-function App() {
-    const courses = [
-        {
-            name: "Half Stack application development",
-            id: 1,
-            parts: [
-                {
-                    name: "Fundamentals of React",
-                    exercises: 10,
-                    id: 1
-                },
-                {
-                    name: "Using props to pass data",
-                    exercises: 7,
-                    id: 2
-                },
-                {
-                    name: "State of a component",
-                    exercises: 14,
-                    id: 3
-                },
-                {
-                    name: "Redux",
-                    exercises: 11,
-                    id: 4
-                }
-            ]
-        },
-        {
-            name: "Node.js",
-            id: 2,
-            parts: [
-                {
-                    name: "Routing",
-                    exercises: 3,
-                    id: 1
-                },
-                {
-                    name: "Middlewares",
-                    exercises: 7,
-                    id: 2
-                }
-            ]
-        }
-    ];
-
+const App = props => {
+    const [notes, setNotes] = useState(props.notes);
+    const [newNote, setNewNote] = useState("");
+    const [showAll, setShowAll] = useState(true);
+    const handleShowAll = () => {
+        setShowAll(!showAll);
+    };
+    const handleNoteChange = event => {
+        setNewNote(event.target.value);
+    };
+    const notesToShow = showAll ? notes : notes.filter(note => note.important);
+    const rows = () =>
+        notesToShow.map(note => <Note key={note.id} note={note} />);
+    const addNote = event => {
+        event.preventDefault();
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() > 0.5,
+            id: notes.length + 1
+        };
+        setNotes(notes.concat(noteObject));
+        setNewNote(" ");
+    };
     return (
         <div>
-            <CourseList courses={courses} />
+            <h1>Notes</h1>
+            <button onClick={handleShowAll}>
+                show {showAll ? "important" : "all"}
+            </button>
+            <ul>{rows()}</ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange} />
+                <button type="submit">save</button>
+            </form>
         </div>
     );
-}
+};
 
 export default App;
