17cecaa29f3925c1f62fd717e75914869a4dee6f
hanameee <hanameee@gmail.com>
2020-01-29 01:03:35 +0900

    Extract fetch code into a separate module
    Axios communicates with backend server.
    All the code related to axios is seperated into independent module (Services/notes).

--- code/src/App.js
+++ code/src/App.js
@@ -1,44 +1,70 @@
 import React, { useState, useEffect } from "react";
+import Note from "./Components/Note";
 import axios from "axios";
-import FilteredResult from "./Components/part2/ex11-14/FilteredResult";
+import noteService from "./Services/notes";
+
 const App = () => {
-    const [countries, setCountries] = useState(null);
-    const [inputValue, setInputValue] = useState("");
-    const [filter, setFilter] = useState("");
-    const [clickedCountry, setClickedCountry] = useState(null);
+    const [notes, setNotes] = useState([]);
+    const [newNote, setNewNote] = useState("");
+    const [showAll, setShowAll] = useState(true);
 
     useEffect(() => {
-        axios.get("https://restcountries.eu/rest/v2/all").then(response => {
-            console.log("fetching....");
-            setCountries(response.data);
+        axios.get("http://localhost:3001/notes").then(returnedData => {
+            setNotes(returnedData);
         });
     }, []);
-    useEffect(() => {
-        setFilter(inputValue.toLowerCase());
-    }, [inputValue]);
-    const inputChangeHandler = event => {
-        setInputValue(event.target.value);
-        setClickedCountry("");
+
+    const notesToShow = showAll ? notes : notes.filter(note => note.important);
+
+    const rows = () =>
+        notesToShow.map(note => (
+            <Note
+                note={note}
+                toggleImportance={() => toggleImportanceOf(note.id)}
+            />
+        ));
+
+    const handleNoteChange = event => {
+        setNewNote(event.target.value);
     };
-    const filteredCountries =
-        countries && filter
-            ? countries.filter(country =>
-                  country.name.toLowerCase().includes(filter)
-              )
-            : null;
-    const showClickHandler = fl => {
-        setClickedCountry(fl);
+
+    const toggleImportanceOf = id => {
+        const note = notes.find(note => note.id === id);
+        const changedNote = { ...note, important: !note.important };
+        noteService
+            .update(note.id, changedNote)
+            .then(returnedNote =>
+                setNotes(
+                    notes.map(note => (note.id !== id ? note : returnedNote))
+                )
+            );
     };
+    const addNote = event => {
+        event.preventDefault();
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() > 0.5
+        };
+        noteService.create(noteObject).then(returnedNote => {
+            setNotes(notes.concat(returnedNote));
+            setNewNote("");
+        });
+    };
+
     return (
         <div>
-            <h1>Country Data</h1>
-            Find countries:
-            <input value={inputValue} onChange={inputChangeHandler} />
-            <FilteredResult
-                props={filteredCountries}
-                clickedCountry={clickedCountry}
-                showClickHandler={showClickHandler}
-            />
+            <h1>Notes</h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? "important" : "all"}
+                </button>
+            </div>
+            <ul>{rows()}</ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange} />
+                <button type="submit">save</button>
+            </form>
         </div>
     );
 };
